d59ed4482072eb656ad59a4a04db18f9018f44d0
sivaram <sivaram0434@gmail.com>
2024-06-23 14:50:19 +0530

    change in docker.sh and installation

--- docker-installation.sh
+++ docker-installation.sh
@@ -4,3 +4,4 @@ systemctl start docker
 usermod -aG docker ec2-user
 systemctl stop docker
 systemctl start docker
+curl -SL https://github.com/docker/compose/releases/download/v2.27.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
--- docker.sh
+++ docker.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 aws ec2 run-instances \
     --image-id ami-08a0d1e16fc3f61ea \
-    --instance-type t3a.medium \
+    --instance-type t3.micro \
     --key-name siva \
     --user-data file:///home/cloudshell-user/bash-scripts/docker-installation.sh \
+    --placement "AvailabilityZone=us-east-1a" \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]' \
     --count 1
