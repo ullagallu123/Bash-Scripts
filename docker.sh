d62bfaf74ea8c01affbd3b0c3fe14aaacc838146
sivaram <sivaram0434@gmail.com>
2024-06-23 14:41:25 +0530

    correcting the path in the docker.sh

--- docker.sh
+++ docker.sh
@@ -3,5 +3,5 @@ aws ec2 run-instances \
     --image-id ami-08a0d1e16fc3f61ea \
     --instance-type t3a.medium \
     --key-name siva \
-    --user-data file:///home/cloudshell-user/bash-scripts \
+    --user-data file:///home/cloudshell-user/bash-scripts/docker-installation.sh \
     --count 1
