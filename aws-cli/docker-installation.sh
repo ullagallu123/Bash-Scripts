2cd651b503dbb66d9f1438877d91e0ea41aea466
sivaram <sivaram0434@gmail.com>
2024-07-14 11:08:05 +0530

    adding bash script to boostrap minkube setup

--- aws-cli/docker-installation.sh
+++ aws-cli/docker-installation.sh
@@ -12,3 +12,11 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 minikube start --network-plugin=cni --cni=calico
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+mv kubectl /usr/local/bin
+
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
