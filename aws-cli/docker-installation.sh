f1475755601000c9c1452f76ff2846d852858af4
sivaram <sivaram0434@gmail.com>
2024-07-17 10:07:35 +0530

    assign tag to it

--- aws-cli/docker-installation.sh
+++ aws-cli/docker-installation.sh
@@ -11,8 +11,6 @@ chmod +x /usr/local/bin/docker-compose
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
-minikube start --network-plugin=cni --cni=calico
-
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
 mv kubectl /usr/local/bin
@@ -20,3 +18,5 @@ mv kubectl /usr/local/bin
 sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
 sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
 sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+# minikube start --network-plugin=cni --cni=calico
--- aws-cli/docker-machine.sh
+++ aws-cli/docker-machine.sh
@@ -4,4 +4,5 @@ aws ec2 run-instances \
   --instance-type t3a.large \
   --key-name siva \
   --user-data file://docker-installation.sh \
-  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}"
+  --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}" \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]'
