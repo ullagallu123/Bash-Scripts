f9cba373513bc41172ae1621ce09be7df5eb668d
sivaram <sivaram0434@gmail.com>
2024-08-09 10:54:38 +0530

    modifying shell script to launch ec2 machine with desired volumeSize

--- aws-cli/docker-machine.sh
+++ aws-cli/docker-machine.sh
@@ -5,4 +5,5 @@ aws ec2 run-instances \
   --key-name siva \
   --user-data file://docker-installation.sh \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}" \
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]'
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]' \
+  --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=20,VolumeType=gp2,DeleteOnTermination=true}'
