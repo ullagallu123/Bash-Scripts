0240223c177da4af7403d38bc2a879826e8689e4
sivaram <sivaram0434@gmail.com>
2024-08-09 12:36:21 +0530

    upding script

--- aws-cli/docker-installation.sh
+++ aws-cli/docker-installation.sh
@@ -5,7 +5,7 @@ systemctl start docker
 usermod -aG docker ec2-user
 systemctl stop docker
 systemctl start docker
-curl -SL https://github.com/docker/compose/releases/download/v2.27.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
