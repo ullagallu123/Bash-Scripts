0eb4cad1a67069de8379cd5fbda0ea394ab427cc
sivaram <sivaram0434@gmail.com>
2024-08-15 16:16:09 +0530

    modi

--- aws-cli/docker-machine.sh
+++ aws-cli/docker-machine.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 aws ec2 run-instances \
   --image-id ami-0a4408457f9a03be3 \
-  --instance-type t3a.large \
+  --instance-type t3a.small \
   --key-name siva \
   --user-data file://docker-installation.sh \
   --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=one-time}" \
   --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=docker}]' \
-  --block-device-mappings 'DeviceName=/dev/sda1,Ebs={VolumeSize=20,VolumeType=gp3,DeleteOnTermination=true}'
+  --block-device-mappings 'DeviceName=/dev/xvda,Ebs={VolumeSize=20,VolumeType=gp3,DeleteOnTermination=true}'
+
